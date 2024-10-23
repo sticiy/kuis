1ca17d2561b8a3b99665eb975db2a668606a1e64
Amefurashi <deathblack691@gmail.com>
2024-10-24 02:26:55 +0700

    OK

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-// Git 2
-
 function app() {
     let answers = {
-        q1: 'soal1-b',
+        q1: 'soal1-b', // Soekarno seharusnya benar untuk soal ini
         q2: 'soal2-a',
         q3: 'soal3-a',
         q4: 'soal4-b',
@@ -11,29 +9,35 @@ function app() {
 
     document.querySelector('#btn').addEventListener('click', () => {
         let score = 0;
+        let allAnswered = true; 
 
-        for(let i = 0; i <= 5; i++) {
-            const ans = document.querySelector(`input[name=soal${i}]:checked`)
-            if(ans) {
-                if(ans.id === answers[`q${i}`]) {
-                    score += 20
-                }
+        for(let i = 1; i <= 5; i++) {
+            const ans = document.querySelector(`input[name=soal${i}]:checked`);
+            if(!ans) {
+                allAnswered = false; 
+                break; 
+            } else if(ans.id === answers[`q${i}`]) {
+                score += 20; 
             }
         }
 
-        document.querySelector('#score').innerHTML = "Score anda : " + score
-    })
+        if (!allAnswered) {
+            document.querySelector('#score').style.color = "red"
+            document.querySelector('#score').innerHTML = "Anda belum menyelesaikan kuis.";
+        } else {
+            
+            document.querySelector('#score').innerHTML = `Skor Anda: ${score}`;
+        }
+    });
 
     document.querySelector('#clear').addEventListener('click', () => {
-        const ans = document.querySelectorAll('input[type="radio"]')
+        const ans = document.querySelectorAll('input[type="radio"]');
         ans.forEach(e => {
             e.checked = false;
         });
 
-        document.querySelector('#score').innerHTML = ""
-    })
-
-
+        document.querySelector('#score').innerHTML = "";
+    });
 }
 
 app();
