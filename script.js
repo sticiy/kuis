f75f6ad4f2eba6d0422199aca4ffb03ed9fe66f7
Amefurashi <deathblack691@gmail.com>
2024-10-23 23:01:49 +0700

    upload

--- script.js
+++ script.js
@@ -6,10 +6,34 @@ function app() {
         q2: 'soal2-a',
         q3: 'soal3-a',
         q4: 'soal4-b',
-        q4: 'soal5-d'
+        q5: 'soal5-d'
     }
 
-    console.log(answers.q1)
+    document.querySelector('#btn').addEventListener('click', () => {
+        let score = 0;
+
+        for(let i = 0; i <= 5; i++) {
+            const ans = document.querySelector(`input[name=soal${i}]:checked`)
+            if(ans) {
+                if(ans.id === answers[`q${i}`]) {
+                    score += 20
+                }
+            }
+        }
+
+        document.querySelector('#score').innerHTML = "Score anda : " + score
+    })
+
+    document.querySelector('#clear').addEventListener('click', () => {
+        const ans = document.querySelectorAll('input[type="radio"]')
+        ans.forEach(e => {
+            e.checked = false;
+        });
+
+        document.querySelector('#score').innerHTML = ""
+    })
+
+
 }
 
 app();
