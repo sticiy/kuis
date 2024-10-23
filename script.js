4f07e5c6f0649d44ba6fedbd0a7a3da2917a8164
Amefurashi <deathblack691@gmail.com>
2024-10-23 21:10:27 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function app() {
+    let answers = {
+        q1: 'soal1-b',
+        q2: 'soal2-a',
+        q3: 'soal3-a',
+        q4: 'soal4-b',
+        q4: 'soal5-d'
+    }
+
+    console.log(answers.q1)
+}
+
+app();
